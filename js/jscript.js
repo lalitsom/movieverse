2c2bb3c74d63c15083975113d743575652302d5f
Lalit-som <Lalitsom27@gmail.com>
2017-09-01 00:45:23 +0530

    changed the design of page

--- js/jscript.js
+++ js/jscript.js
@@ -1,50 +1,66 @@
-moviedb_api_key = "aa8b43b8cbce9d1689bef3d0c3087e4d"; // themoviedb.org api key
+var moviedbApiKey = 'aa8b43b8cbce9d1689bef3d0c3087e4d' // themoviedb.org api key
+var searchCategory = 'movie'  // category could be movie, person or tv
 
-//initializing a ajax object to request api
+//  initializing a ajax object to request api
 if (window.XMLHttpRequest) {
-    // code for IE7+, Firefox, Chrome, Opera, Safari
-    xmlhttpObject = new XMLHttpRequest();
+  // code for IE7+, Firefox, Chrome, Opera, Safari
+  var xmlhttpObject = new window.XMLHttpRequest()
 } else {
-    // code for IE6, IE5
-    xmlhttpObject = new ActiveXObject("Microsoft.XMLHTTP");
+  // code for IE6, IE5
+  xmlhttpObject = new window.ActiveXObject('Microsoft.XMLHTTP')
 }
 
-
-//whenever ajax object get some valid response show it in browser
-xmlhttpObject.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      updateList(this.response);
-    }
+//  whenever ajax object get some valid response show it in browser
+xmlhttpObject.onreadystatechange = function () {
+  if (this.readyState === 4 && this.status === 200) {
+    updateList(this.response)
   }
-
-//create and send the query to themoviedb.org
-function get_Json_Data(){
-  query = document.getElementById('query_input').value;
-  if(query.length<3){
-    return 0;
-  }
-  xmlhttpObject.open("GET","https://api.themoviedb.org/3/search/movie?api_key="+moviedb_api_key+"&query=" +
-    query,
-    true);
-  xmlhttpObject.send();
 }
 
-jsonObj ="";
-function updateList(jsonData){
-    jsonObj = JSON.parse(jsonData);
-    total_results = jsonObj.total_results;
-    if(total_results>10){
-      total_results =10;
-    }
-    console.log(total_results);
-    resultList = document.getElementById('result_list');
-    result_str="";
-    img_baseurl = "http://image.tmdb.org/t/p/w185/";
-    for(let i=0;i<total_results;i++){
-      img_path = img_baseurl + jsonObj.results[i].poster_path;
-      console.log(img_path);
-      result_str += "<li class='movie_element'><img src="+img_path+"> <h3>"+jsonObj.results[i].title+"</h3>"+jsonObj.results[i].overview+"</li>"
-    }
+//  check if user pressed enter
+function getList (kbdEvent) {
+  var query = document.getElementById('query_input').value
+  if (kbdEvent.code === 'Enter' && query.length > 2) {
+    getJsonData(query)
+  }
+  if (query.length === 0) {
+    document.getElementById('result_list').innerHTML = ''
+  }
+}
 
-    resultList.innerHTML = result_str;
+//  create and send the query to themoviedb.org
+function getJsonData (query) {
+  xmlhttpObject.open('GET', 'https://api.themoviedb.org/3/search/' + searchCategory + '?api_key=' + moviedbApiKey +
+  '&query=' + query, true)
+  xmlhttpObject.send()
+}
+
+var jsonObj = ''
+function updateList (jsonData) {
+  jsonObj = JSON.parse(jsonData)
+  var totalResults = jsonObj.total_results
+  if (totalResults > 20) {
+    totalResults = 20
+  }
+
+  var resultList = document.getElementById('result_list')
+  var resultStr = ''
+  var imgBaseUrl = 'http://image.tmdb.org/t/p/w185/'
+  for (let i = 0; i < totalResults; i++) {
+    if ((jsonObj.results[i].poster_path || jsonObj.results[i].profile_path) == null) {
+      continue
+    }
+    var imgPath = imgBaseUrl + (jsonObj.results[i].poster_path || jsonObj.results[i].profile_path)
+    resultStr += '<li class="movie_element"><img src=' +
+    imgPath + '> <h3>' + (jsonObj.results[i].title || jsonObj.results[i].name) + '</h3></li>'
+  }
+
+  resultList.innerHTML = resultStr
+}
+
+function setCategory (option) {
+  searchCategory = option.dataset.category
+  document.getElementById('query_input').placeholder = 'Search ' + option.innerHTML + '...'
+  document.getElementById('query_input').value = ''
+  document.getElementById('result_list').innerHTML = ''
 }
